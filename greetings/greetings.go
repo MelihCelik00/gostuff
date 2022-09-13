2d213e04a4ad7c583d5c3f1df2daacda72b46ce5
MelihCelik00 <melihsafa.c@gmail.com>
2022-09-13 15:51:35 +0300

    multiple return stufffff

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,10 +1,44 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
-	msg:=fmt.Sprintf("Hi, %v. Welcome!", name)
-	return msg
+func Hello(name string) (string,error) {
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+	msg:=fmt.Sprintf(randomFormat(), name)
+	return msg, nil
+}
+
+func Hellos (names []string) (map[string]string, error) {
+	messages := make(map[string]string)
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		messages[name] = message
+	}
+	return messages, nil
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomFormat() string {
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	return formats[rand.Intn(len(formats))]
 }
 
